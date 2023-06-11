98ea3e2f6d054e9b3d7f3932880a1730b7d0b880
getAlexRibeiro <alexribeiroperola@gmail.com>
2023-06-11 15:01:03 +0200

    add main

---
+++ main.py
@@ -0,0 +1,5 @@
+import script_auto_alex
+
+if __name__ == '__main__':
+    sa = script_auto_alex
+    sa.minar()
--- script_auto_alex.py
+++ script_auto_alex.py
@@ -4,31 +4,23 @@ import keyboard
 import random
 
 
-x_box, y_box = 1791, 690
-x_fill, y_fill = 1791, 723
+def minar():
+    # Coordenadas para res 1920x1080
+    x_box, y_box = 1791, 690
 
+    stop = True
+    current_x, current_y = pyautogui.size()
+    target_x = current_x // 2
+    target_y = current_y // 2
 
-
-isPressed = True
-current_x, current_y = pyautogui.size()
-target_x = current_x // 2
-target_y = current_y // 2
-
-num_clicks = 7
-
-delay_click = random.randint(1,5)
-delay_fill = random.randint(100,120)
-
-
-for i in range(delay_fill):
-	time.sleep(1)
-	pyautogui.click(x_box, y_box, button='right')
-	time.sleep(1)
-	pyautogui.click(x_fill, y_fill)
-	for j in range(num_clicks):
-		time.sleep(1)
-		pyautogui.click(target_x, target_y)
-		time.sleep(delay_click)
-
-	if keyboard.is_pressed('f'):
-		break
+    # delay_click = random.randint(1, 20)ff
+    # delay_fill = random.randint(1, 10)
+    while stop:
+        for j in range(random.randint(1, 10)):
+            if keyboard.is_pressed('F'):
+                stop = False
+                break
+            pyautogui.click(x_box, y_box)
+            pyautogui.click(x_box, y_box)
+        time.sleep(random.randint(1, 5))
+        pyautogui.click(target_x, target_y)
